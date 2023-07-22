ac4eeee1f18bcdec495c637c9a6f2e48176be72e
Adinoyi Sadiq <sadiqadinoyi@gmail.com>
2023-07-22 11:55:13 +0100

    Update optimization implementation

--- model/neural-net.py
+++ model/neural-net.py
@@ -109,20 +109,20 @@ activation2 = Activation_Softmax()
 loss_function = Loss_CategoricalCrossentropy()
 
 # Helper variables
-lowest_loss = 9999999 
+lowest_loss = 9999999
 
 # some initial value 
-best_dense1_weights = dense1.weights.copy() 
-best_dense1_biases = dense1.biases.copy() 
-best_dense2_weights = dense2.weights.copy() 
+best_dense1_weights = dense1.weights.copy()
+best_dense1_biases = dense1.biases.copy()
+best_dense2_weights = dense2.weights.copy()
 best_dense2_biases = dense2.biases.copy()
 
 for iteration in range(10000):
 
   # Update weights with some small random values
-  dense1.weights += 0.05 * np.random.randn(2, 3) 
-  dense1.biases += 0.05 * np.random.randn(1, 3) 
-  dense2.weights += 0.05 * np.random.randn(3, 3) 
+  dense1.weights += 0.05 * np.random.randn(2, 3)
+  dense1.biases += 0.05 * np.random.randn(1, 3)
+  dense2.weights += 0.05 * np.random.randn(3, 3)
   dense2.biases += 0.05 * np.random.randn(1, 3)
 
   # Perform a forward pass of our training data through this layer
@@ -151,29 +151,25 @@ for iteration in range(10000):
   # it takes the output of second dense layer here and returns loss 
   loss = loss_function.calculate(activation2.output, y)
 
-  # Print loss value
-  print('loss:', loss)
-
-  # Calculate accuracy from output of activation2 and targets # calculate values along first axis
+  # Calculate accuracy from output of activation2 and targets 
+  # calculate values along first axis
   predictions = np.argmax(activation2.output, axis=1)
   if len(y.shape) == 2:
     y = np.argmax(y, axis=1) 
-  accuracy = np.mean(predictions == y)
-    
-  # Print accuracy
-  print('acc:', accuracy)
+  accuracy = np.mean(predictions==y)
 
   # If loss is smaller - print and save weights and biases aside
   if loss < lowest_loss:
-    print('New set of weights found, iteration:', iteration, 'loss:', loss, 'acc:', accuracy) 
-    best_dense1_weights = dense1.weights.copy() 
-    best_dense1_biases = dense1.biases.copy() 
-    best_dense2_weights = dense2.weights.copy() 
-    best_dense2_biases = dense2.biases.copy() 
+    print('New set of weights found, iteration:', iteration,
+          'loss:', loss, 'acc:', accuracy)
+    best_dense1_weights = dense1.weights.copy()
+    best_dense1_biases = dense1.biases.copy()
+    best_dense2_weights = dense2.weights.copy()
+    best_dense2_biases = dense2.biases.copy()
     lowest_loss = loss
   # Revert weights and biases
   else:
-    dense1.weights = best_dense1_weights.copy() 
-    dense1.biases = best_dense1_biases.copy() 
-    dense2.weights = best_dense2_weights.copy() 
+    dense1.weights = best_dense1_weights.copy()
+    dense1.biases = best_dense1_biases.copy()
+    dense2.weights = best_dense2_weights.copy()
     dense2.biases = best_dense2_biases.copy()
