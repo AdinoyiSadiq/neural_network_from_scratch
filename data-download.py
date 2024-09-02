29fa39091697ff552e8007b4e69fd45b8ae2108b
Adinoyi Sadiq <sadiqadinoyi@gmail.com>
2024-09-02 13:11:27 +0100

    Finishes: Add repo setup - Add folder check in data-download.py file - Update gitignore to account for the change in virtual env name to nnfs

--- data-download.py
+++ data-download.py
@@ -7,6 +7,11 @@ URL = 'https://nnfs.io/datasets/fashion_mnist_images.zip'
 FILE = '../data/fashion_mnist_images.zip'
 FOLDER = 'fashion_mnist_images'
 
+# Ensure the directory exists
+if not os.path.exists(os.path.dirname(FILE)):
+    os.makedirs(os.path.dirname(FILE))
+
+# Ensure the file exists
 if not os.path.isfile(FILE):
   print(f'Downloading {URL} and saving as {FILE}...') 
   urllib.request.urlretrieve(URL, FILE)
